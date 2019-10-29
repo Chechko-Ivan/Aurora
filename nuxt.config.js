af6ce2c006dabd660ef3269de0377473822ca249
Chechko Ivan <chechkovania@gmail.com>
2019-10-29 22:15:37 +0300

    initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,77 @@
+export default {
+  mode: 'spa',
+
+  head: {
+    title: 'АВРОРАПЛАСТ',
+    meta: [
+      {
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
+    ],
+    link: [
+      {
+        rel: 'apple-touch-icon',
+        href: '/favicon/apple-touch-icon.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon/favicon-32x32.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon/favicon-16x16.png'
+      },
+      {
+        rel: 'mask-icon',
+        color: '#5bbad5',
+        href: '/favicon/safari-pinned-tab.svg'
+      },
+      {
+        rel: 'msapplication-TileColor',
+        content: '#2b5797'
+      },
+      {
+        rel: 'theme-color',
+        content: '#ffffff'
+      }
+    ]
+  },
+
+  loading: {
+    color: '#F16922'
+  },
+
+  css: ['~/assets/style/index.scss'],
+
+  plugins: ['~/plugins/antd.js'],
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module'
+  ],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [],
+  /*
+   ** Build configuration
+   */
+  build: {
+    extend(config, ctx) {}
+  }
+}
